e146f2d3e5a6a23264ac029173ee9e908d65e7ba
Francisco Cerda <francisco.cerda@gmail.com>
2020-07-29 22:58:29 -0400

    final

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,18 @@
 $(document).ready(function () {
+  var headerH = document.getElementsByTagName("header")[0].clientHeight;
   $("a").on("click", function (e) {
     if (this.hash !== "" && this.hash !== "#carouselExampleCaptions") {
       e.preventDefault();
       var hash = this.hash;
+      var scroll = $(hash).offset().top - headerH;
       $("html, body").animate(
         {
-          scrollTop: $(hash).offset().top,
+          scrollTop: scroll,
         },
         800,
         function () {
           window.location.hash = hash;
+          $("html").animate({ scrollTop: scroll }, 0);
         }
       );
     }
